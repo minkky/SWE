dcd5a75d10fe4134046dbd5a2745f23c0b236ee2
minkky <minjiseo95@gmail.com>
2016-11-23 15:23:42 +0900

    total layout file

---
+++ MGJS/app/src/main/java/com/example/mgjs/mgjs/PhoneBookActivity.java
@@ -0,0 +1,13 @@
+package com.example.mgjs.mgjs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneBookActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonebook);
+    }
+}
