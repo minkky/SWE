49eb53fd8c81b5ff59dc2bbf4af0f0f20e77cc98
unknown <estanie@sookmyung.ac.kr>
2016-11-28 04:51:34 +0900

    design activity_note & activity_schedule. add default Action in noteActivity and scheduleActivity.

--- MGJS/app/src/main/java/com/example/mgjs/mgjs/NoteActivity.java
+++ MGJS/app/src/main/java/com/example/mgjs/mgjs/NoteActivity.java
@@ -1,13 +1,29 @@
 package com.example.mgjs.mgjs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class NoteActivity extends AppCompatActivity {
+
+    ImageButton note_addButton;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        note_addButton = (ImageButton)findViewById(R.id.note_addButton);
+        note_addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(NoteActivity.this, Add_NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
