4907fa7a7dc46bf27edb2e9bc12633c94ce8d9ec
minkky <minjiseo95@gmail.com>
2016-11-27 18:22:59 +0900

    login by minji

--- MGJS/app/src/main/java/com/example/mgjs/mgjs/LoginActivity.java
+++ MGJS/app/src/main/java/com/example/mgjs/mgjs/LoginActivity.java
@@ -1,13 +1,46 @@
 package com.example.mgjs.mgjs;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText id, pw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        id = (EditText)findViewById(R.id.login_id);
+        pw = (EditText)findViewById(R.id.login_pw);
+
+    }
+
+    public void loginClicked(View view){
+        if(id.getText().toString().equals(" ") && pw.getText().toString().equals(" ")){
+            Toast.makeText(getApplicationContext(),"로그인 성공",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("ID/PW를 확인해주세요");
+            alertDialogBuilder.setPositiveButton("확인",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            ;
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
 }
--- MGJS/app/src/main/java/com/example/mgjs/mgjs/MainActivity.java
+++ MGJS/app/src/main/java/com/example/mgjs/mgjs/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mgjs.mgjs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,23 +13,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    TextView schedule, phonebook, note, shortcut_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shortcutmenu);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        schedule = (TextView)findViewById(R.id.main_scheduletv);
+        schedule.setText("스케쥴 관리");
+        schedule.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
+        phonebook = (TextView)findViewById(R.id.main_phonebooktv);
+        phonebook.setText("전화번호부");
+        phonebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PhoneBookActivity.class);
+                startActivity(intent);
+            }
+        });
+        note = (TextView)findViewById(R.id.main_notetv);
+        note.setText("메모");
+        note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -38,8 +61,13 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        shortcut_id = (TextView)findViewById(R.id.shortcut_id);
+        if(shortcut_id != null)
+            Toast.makeText(getApplicationContext(), shortcut_id.getText().toString(), Toast.LENGTH_SHORT).show();
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -52,47 +80,20 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        if (id == R.id.shortcut_schedule) {
+            intent = new Intent(MainActivity.this, ScheduleActivity.class);
+        } else if (id == R.id.shortcut_phonebook) {
+            intent = new Intent(MainActivity.this, PhoneBookActivity.class);
+        } else if (id == R.id.shortcut_memo) {
+            intent = new Intent(MainActivity.this, NoteActivity.class);
         }
+        startActivity(intent);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
